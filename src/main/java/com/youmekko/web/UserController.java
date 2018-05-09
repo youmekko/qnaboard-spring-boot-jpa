35dbc244a7ab94e6a115e471777d2ebdd9c37a68
youmekko <youmekko1129@gmail.com>
2018-05-09 12:26:37 +0900

    Feat : URL refactoring

--- src/main/java/com/youmekko/web/UserController.java
+++ src/main/java/com/youmekko/web/UserController.java
@@ -1,29 +1,47 @@
 package com.youmekko.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.youmekko.domain.User;
+import com.youmekko.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-	
-	private List<User> users = new ArrayList<User>();
 
-	@PostMapping("/create")
-	public String create(User user) {
-		System.out.println(user);
-		this.users.add(user);
-		return "redirect:/list";
+	@Autowired
+	private UserRepository userRepository;
+
+	@GetMapping("/form")
+	public String form() {
+		return "/user/form";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "/user/login";
+	}
+	
+	@GetMapping("/profile")
+	public String profile() {
+		return "/user/profile";
 	}
 
-	
-	@GetMapping("/list")
+	@PostMapping("")
+	public String create(User user) {
+		System.out.println(user);
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+
+	@GetMapping("")
 	public String list(Model model) {
-		model.addAttribute("users", this.users);
-		return "list";
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list";
 	}
 }
--- src/main/java/com/youmekko/web/WelcomeController.java
+++ src/main/java/com/youmekko/web/WelcomeController.java
@@ -1,18 +1,16 @@
 package com.youmekko.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class WelcomeController {
 
-	@GetMapping("/welcome")
-	public String welcome(String name, int age, Model model) {
-		System.out.println("name : " + name + " age: " + age);
-		model.addAttribute("name", name);
-		model.addAttribute("age", age);
-		return "welcome";
+	@GetMapping("")
+	public String index() {
+		return "index";
 	}
 
 }
