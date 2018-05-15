62288ad477ca0f27ee5d9b9122d1b57dd9593e4d
youmekko <youmekko1129@gmail.com>
2018-05-15 16:45:07 +0900

    Feat : question detail view

--- src/main/java/com/youmekko/web/QuestionController.java
+++ src/main/java/com/youmekko/web/QuestionController.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,7 +17,7 @@ import com.youmekko.domain.User;
 @Controller
 @RequestMapping("/questions")
 public class QuestionController {
-	
+
 	@Autowired
 	private QuestionRepository questionRepository;
 
@@ -34,13 +36,19 @@ public class QuestionController {
 		if (!HttpSessionUtils.isLoginUser(session)) {
 			return "/user/login";
 		}
-		
+
 		User loginedUser = HttpSessionUtils.getUserFromSession(session);
 
 		Question newQuestion = new Question(loginedUser, title, contents);
 		questionRepository.save(newQuestion);
-		
+
 		return "redirect:/";
+	}
+
+	@GetMapping("/{id}")
+	public String showDetail(@PathVariable Long id, Model model) {
+		model.addAttribute("question", questionRepository.findById(id).get());
+		return "/qna/show";
 	}
 
 }
