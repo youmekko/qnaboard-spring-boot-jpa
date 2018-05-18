aca46effdfd2b202f48e4b40a07abf6acc7607e7
youmekko <youmekko1129@gmail.com>
2018-05-18 12:40:05 +0900

    Feat : prevent comment-write submit (jquery)

--- src/main/java/com/youmekko/web/ApiCommentController.java
+++ src/main/java/com/youmekko/web/ApiCommentController.java
@@ -3,10 +3,10 @@ package com.youmekko.web;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.youmekko.domain.Comment;
 import com.youmekko.domain.CommentRepository;
@@ -14,9 +14,9 @@ import com.youmekko.domain.Question;
 import com.youmekko.domain.QuestionRepository;
 import com.youmekko.domain.User;
 
-@Controller
-@RequestMapping("/questions/{questionId}/comments")
-public class CommentController {
+@RestController
+@RequestMapping("/api/questions/{questionId}/comments")
+public class ApiCommentController {
 
 	@Autowired
 	private QuestionRepository questionRepository;
@@ -25,18 +25,17 @@ public class CommentController {
 	private CommentRepository commentRepository;
 
 	@PostMapping("")
-	public String create(@PathVariable Long questionId, String contents, HttpSession session) {
+	public Comment create(@PathVariable Long questionId, String contents, HttpSession session) {
 		if (!HttpSessionUtils.isLoginUser(session)) {
-			return "/user/login";
+			return null;
 		}
 
 		User loginedUser = HttpSessionUtils.getUserFromSession(session);
 		Question question = questionRepository.findById(questionId).get();
-
 		Comment comment = new Comment(loginedUser, question, contents);
-		commentRepository.save(comment);
+		
+		return commentRepository.save(comment);
 
-		return String.format("redirect:/questions/%d", questionId);
 	}
 
 }
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,9 +1,29 @@
-String.prototype.format = function() {
-  var args = arguments;
-  return this.replace(/{(\d+)}/g, function(match, number) {
-    return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-        ;
-  });
-};
+$(".comment-write input[type=submit]").click(addComment);
+
+function addComment(e) {
+	e.preventDefault();
+	console.log("click!");
+	
+   var queryString = $(".comment-write").serialize();
+   console.log("query : " + queryString);
+   
+   var url = $(".comment-write").attr("action");
+   console.log("url : " + url);
+   
+   $.ajax({
+	    type : 'post',
+	    url : url,
+	    data : queryString,
+	    dataType : 'json',
+	    error : onError,
+	    success : onSuccess});
+	
+}
+
+function onError(){ 
+	
+}
+
+function onSuccess(data, status){
+	console.log(data);
+}
